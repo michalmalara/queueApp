e0a271d4e7a03f48a9aa8d22f94a9f07e7b56b08
michal <michalmalara@gmail.com>
2023-09-03 08:48:16 +0200

    feat(queueService) station and case models created

--- queueService/admin.py
+++ queueService/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from queueService.models import Station, Case
+
 # Register your models here.
+
+admin.site.register(Station)
+admin.site.register(Case)
--- queueService/models.py
+++ queueService/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', related_name='stations', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"Station {self.name}"
+
+
+class Case(models.Model):
+    name = models.CharField(max_length=100)
+    stations = models.ManyToManyField(Station, related_name='cases')
+
+    def __str__(self):
+        return f"Case {self.name}"
