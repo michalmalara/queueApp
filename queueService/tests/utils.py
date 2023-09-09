326f68a772e2139a0c56662fef4a2d1c8a5c61d1
michal <michalmalara@gmail.com>
2023-09-09 16:02:57 +0200

    feat(queueService) type hints added to test util

--- queueService/tests/utils.py
+++ queueService/tests/utils.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 
+User = get_user_model()
 
-def get_token_for_user(user):
+
+def get_token_for_user(user: User) -> str:
     refresh = RefreshToken.for_user(user)
 
     return f"Bearer {refresh.access_token}"
