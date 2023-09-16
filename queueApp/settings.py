3cb9a7804a7e2e67305d0c2c26e0b777ee963bca
michal <michalmalara@gmail.com>
2023-09-16 12:32:03 +0200

    feat(frontend) Angular frontend initialized

--- queueApp/settings.py
+++ queueApp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "queueApp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"]
+        "DIRS": [os.path.join(BASE_DIR, "static")]
         ,
         "APP_DIRS": True,
         "OPTIONS": {
@@ -146,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- queueApp/urls.py
+++ queueApp/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path(r'', TemplateView.as_view(template_name="templates/home.html")),
     path("admin/", admin.site.urls),
     path("api/", include("queueService.api.urls")),
 ]
